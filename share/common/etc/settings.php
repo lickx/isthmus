adebc50295a6f4bdf85aa5829f6d69a2a82049b4
lickx <sinesurfer@gmail.com>
2020-12-27 18:26:26 +0100

    Forgot settings.php example

---
+++ share/common/etc/settings.php
@@ -0,0 +1,14 @@
+<?php
+// these settings are for the optional web services
+
+$dbserver = "localhost";
+$dbuser = "isthmus";
+$dbpassword = "SECRET";
+$dbname = "isthmus";
+
+$grid_uri = "hg.example.com:8002";
+$welcome_name = "Welcome";
+$welcome_x = "127";
+$welcome_y = "127";
+$welcome_z = "21";
+?>
