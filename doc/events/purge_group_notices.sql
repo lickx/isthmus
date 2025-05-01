0c4f2f31b2450a5490900cb72637bff4b5c26cea
lickx <11088547+lickx@users.noreply.github.com>
2025-05-01 13:04:30 +0200

    Move grppurge functionality to a sql event

---
+++ doc/events/purge_group_notices.sql
@@ -0,0 +1,10 @@
+-- First, as mysql user root, SET GLOBAL event_scheduler=ON;
+
+-- To be sourced into your robust database
+
+CREATE EVENT IF NOT EXISTS `purge_group_notices`
+ON SCHEDULE
+  EVERY 1 DAY
+  COMMENT 'Purges group notices older than 14 days'
+  DO
+    DELETE FROM os_groups_notices WHERE TMStamp < UNIX_TIMESTAMP()-1209600;
